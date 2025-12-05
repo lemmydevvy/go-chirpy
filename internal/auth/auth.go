3f0a12f12e64e609d2290c6486c5bc069c82f434
mdnewmandev <mdnewman@proton.me>
2025-12-05 19:39:31 +0000

    ADD some jwt

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,13 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -20,4 +26,50 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 	}
 
 	return match, nil
+}
+
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	signingKey := []byte(tokenSecret)
+	now := time.Now().UTC()
+
+	claims := &jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(now),
+		ExpiresAt: jwt.NewNumericDate(now.Add(expiresIn)),
+		Subject: userId.String(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	s, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to make JWT: %w", err)
+	}
+
+	return s, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("token is invalid or has expired: %w", err)
+	} 
+	
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("token is invalid")
+	}
+
+	userIDString := claims.Subject
+	issuer := claims.Issuer
+	if issuer != string("chirpy") {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+
+	id, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid user ID: %w", err)
+	}
+	return id, nil
 }
